02b7841e5003acd88244175ff6172a22d332321c
Arpit <omagrahari55@gmail.com>
2025-07-13 17:52:51 +0530

    api done and it's creating nre job form and  saving it on db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 import "./cron/jobCron.js"
+import { connectdb } from "./db/db.js";
+connectdb();
 app.listen(process.env.PORT ,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
 })
