7272cd3773f1bae8384995518a2ddc1cc472309d
Arpit <omagrahari55@gmail.com>
2025-07-18 18:44:05 +0530

    build fix  -5

--- cron/jobCron.js
+++ cron/jobCron.js
@@ -2,9 +2,9 @@ import cron from "node-cron";
 import { SendMail } from "../controller/Controller.js";
 import { dbchecker } from "../controller/jobchecker.js";
 
-cron.schedule("*/5 * * * *", async () => {
+cron.schedule("*/2 * * * *", async () => {
 //   SendMail()
     // console.log("Checking the db to send emails");
    await dbchecker();
-    console.log("Cron job is running every 5 minutes");
+    console.log("Cron job is running every 2 minutes");
 });
