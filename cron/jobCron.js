243b765b99405f015642ed522e9e3a4b74f22a38
Arpit <omagrahari55@gmail.com>
2025-07-18 16:08:51 +0530

    changed the cron job time to 10min

--- cron/jobCron.js
+++ cron/jobCron.js
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { SendMail } from "../controller/Controller.js";
 import { dbchecker } from "../controller/jobchecker.js";
 
-cron.schedule("*/10 * * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
 //   SendMail()
     // console.log("Checking the db to send emails");
    await dbchecker();
