087718507adb5fca446989df09da0ff3a2b95678
Arpit <omagrahari55@gmail.com>
2025-07-22 17:58:09 +0530

    internshalla job/internship scraper done and job cron set to 10min

--- cron/jobCron.js
+++ cron/jobCron.js
@@ -7,7 +7,7 @@ cron.schedule("*/10 * * * *", async () => {
 //   SendMail()
     // console.log("Checking the db to send emails");
     try {
-        console.log("Cron job is running every 2 minutes");
+        console.log("Cron job is running every 10 minutes");
         await dbchecker();
         //  await internshala_scraper()
         
