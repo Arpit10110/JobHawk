7baf7c8c8b2f593eefa96b1e867ceca29bf86c07
Arpit <omagrahari55@gmail.com>
2025-09-03 18:19:25 +0530

    bug fixed 4 naukari

--- puppeteer/Naukarijob_Api.js
+++ puppeteer/Naukarijob_Api.js
@@ -5,27 +5,25 @@ const get_jobs = async (data) => {
   try {
     // IMPORTANT: Update this cookie string regularly from a valid logged-in session
     const cookieString = "test=naukri.com; _t_us=...; ak_bmsc=...; bm_sv=...; jd=...; ...";
-
     const headers = {
-      accept: "application/json",
-      "accept-language": "en-US,en;q=0.9",
-      appid: "109",
-      systemid: "Naukri",
-      clientid: "d3skt0p",
-      "content-type": "application/json",
-      referer:
-        "https://www.naukri.com/front-end-backend-development-jobs?k=front%20end%2C%20backend%20development",
-      "user-agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-      cookie: cookieString,
-      "sec-ch-ua": `"Not;A=Brand";v="99", "Brave";v="139", "Chromium";v="139"`,
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": `"Windows"`,
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      // Remove 'Accept-Encoding' from headers to let axios handle it automatically
-    };
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        appid: "109",
+        systemid: "Naukri",
+        clientid: "d3skt0p",
+        "content-type": "application/json",
+        referer: "https://www.naukri.com/fullstack-developer-frontend-development-jobs?k=fullstack%20developer%2C%20frontend%20development&experience=2&nignbevent_src=jobsearchDeskGNB",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+        authorization: "Bearer eyJraWQiOiIzIiwidHlwIjoiSldUIiwiYWxnIjoiUlM1MTIifQ.eyJkZXZpY2VUeXBlIjoiZDNza3QwcCIsInVkX3Jlc0lkIjozMDQ5OTgzODgsInN1YiI6IjMxNjA1MTIyMiIsInVkX3VzZXJuYW1lIjoieGFiaXZvODUxNEBmdWFzaGEuY29tIiwidWRfaXNFbWFpbCI6dHJ1ZSwiaXNzIjoiSW5mb0VkZ2UgSW5kaWEgUHZ0LiBMdGQuIiwidXNlckFnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEzOS4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiaXBBZHJlc3MiOiIxNTcuNDkuNy4wIiwidWRfaXNUZWNoT3BzTG9naW4iOmZhbHNlLCJ1c2VySWQiOjMxNjA1MTIyMiwic3ViVXNlclR5cGUiOiJqb2JzZWVrZXIiLCJ1c2VyU3RhdGUiOiJBVVRIRU5USUNBVEVEIiwidWRfaXNQYWlkQ2xpZW50IjpmYWxzZSwidWRfZW1haWxWZXJpZmllZCI6ZmFsc2UsInVzZXJUeXBlIjoiam9ic2Vla2VyIiwic2Vzc2lvblN0YXRUaW1lIjoiMjAyNS0wOS0wM1QxNzo0NDo0NiIsInVkX2VtYWlsIjoieGFiaXZvODUxNEBmdWFzaGEuY29tIiwidXNlclJvbGUiOiJ1c2VyIiwiZXhwIjoxNzU2OTA1Mjg2LCJ0b2tlblR5cGUiOiJhY2Nlc3NUb2tlbiIsImlhdCI6MTc1NjkwMTY4NiwianRpIjoiOGE5YjgwZjA5NzkzNDM2ZGE2YzIwOWMyOTMyYzMxZGUiLCJwb2RJZCI6InByb2QtNTRiOGM2ZDk4LTRicDhuIn0.VMZCnfX0ZBL7C58xd8m7XjjMh88N_Qa_UwgUHutNQlvr8vUUW55UJ4WtNRqGNnFJztXVTCC9oSb7hpBMWAivR21J-SO71Llos7DuyjnjPqLSoXvTebOMw-MC3DFfbdr6vuUkqt-sZMSOLb6hQHvviWDmgkCaunyETjAF2xTeK3AAWpqbGiRQM4goHcbjLdCk1oOv_Dz1OlplVDVVgpPXmafy_v3b7do4ZSZpTGZXMTmsyX5LL-ExycgEHojSBzkDhJ4yK7kpBGL3WEEI9cDQwhvlzt2hRBwOKZqQsqwNGBXRrFyKTYhIhosOq2bkQOAc2bd6daDK3x4YuBltpnJJlg",
+        cookie: cookieString,
+        "sec-ch-ua": `"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"`,
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": `"Windows"`,
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+      };
+      
 
     const url = await generate_url(data);
 
